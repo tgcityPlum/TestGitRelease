8767b06c710c5cd2f30c4e9bdf44a57235513f07
chenwei <wei.chen@uzer.me>
2019-08-30 14:59:09 +0800

    add project

---
+++ mylibrary/src/main/java/com/tgcity/mylibrary/Testclass.java
@@ -0,0 +1,15 @@
+package com.tgcity.mylibrary;
+
+import android.util.Log;
+
+/**
+ * @author TGCity
+ * @date 2019/8/30
+ * @describe
+ */
+public class Testclass {
+
+    public static void test() {
+        Log.e("Testclass", "test");
+    }
+}
